5f8111cb3662eecda57d837b8e87d42a72d9fda3
lucasgabrielsantos <lucas.gsantos@live.com>
2020-04-15 23:32:22 -0300

    uticriando umo primeiro modulo do koin para a injecao de dependencia no banco de dados local

--- android-tech-news-projeto-inicial/app/src/main/java/br/com/alura/technews/ui/activity/FormularioNoticiaActivity.kt
+++ android-tech-news-projeto-inicial/app/src/main/java/br/com/alura/technews/ui/activity/FormularioNoticiaActivity.kt
@@ -15,6 +15,7 @@ import br.com.alura.technews.util.mostraErro
 import br.com.alura.technews.viewmodel.FormularioNoticiaViewModel
 import br.com.alura.technews.viewmodel.factory.FormularioNoticiaViewModelFactory
 import kotlinx.android.synthetic.main.activity_formulario_noticia.*
+import org.koin.android.ext.android.inject
 
 private const val TITULO_APPBAR_EDICAO = "Editando notícia"
 private const val TITULO_APPBAR_CRIACAO = "Criando notícia"
@@ -22,12 +23,13 @@ private const val MENSAGEM_ERRO_SALVAR = "Não foi possível salvar notícia"
 
 class FormularioNoticiaActivity : AppCompatActivity() {
 
+    private val database: AppDatabase by inject()
     private val noticiaId: Long by lazy {
         intent.getLongExtra(NOTICIA_ID_CHAVE, 0)
     }
 
     private val viewModel: FormularioNoticiaViewModel by lazy {
-        val repository = NoticiaRepository(AppDatabase.getInstance(this).noticiaDAO)
+        val repository = NoticiaRepository(database.noticiaDAO)
         val factory = FormularioNoticiaViewModelFactory(repository)
         ViewModelProviders.of(this, factory).get(FormularioNoticiaViewModel::class.java)
     }
