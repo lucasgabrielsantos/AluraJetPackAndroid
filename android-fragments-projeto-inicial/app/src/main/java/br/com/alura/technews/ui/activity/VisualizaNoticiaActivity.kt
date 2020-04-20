ab0b40c36db7561957d1a7dfbd28d749e5f68b8e
lucasgabrielsantos <lucas.gsantos@live.com>
2020-04-20 11:54:56 -0300

    criando um fragment de maneira programavel

--- android-fragments-projeto-inicial/app/src/main/java/br/com/alura/technews/ui/activity/VisualizaNoticiaActivity.kt
+++ android-fragments-projeto-inicial/app/src/main/java/br/com/alura/technews/ui/activity/VisualizaNoticiaActivity.kt
@@ -20,7 +20,15 @@ class VisualizaNoticiaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_visualiza_noticia)
         title = TITULO_APPBAR
+        val transacao = supportFragmentManager.beginTransaction()
+        val fragment = VisualizaNoticiaFragment()
+        val dados = Bundle()
+        dados.putLong(NOTICIA_ID_CHAVE, noticiaId)
+        fragment.arguments = dados
+        transacao.add(R.id.activity_visualiza_noticia_container, fragment)
+        transacao.commit()
     }
+
 
     override fun onAttachFragment(fragment: Fragment?) {
         super.onAttachFragment(fragment)
