e74722b9dee5f717b834d1cbe97632ea62539f27
lucasgabrielsantos <lucas.gsantos@live.com>
2020-04-21 00:41:24 -0300

    evitando problemas com a rotação de tela pelo tablet

--- android-fragments-projeto-inicial/app/src/main/java/br/com/alura/technews/ui/activity/NoticiasActivity.kt
+++ android-fragments-projeto-inicial/app/src/main/java/br/com/alura/technews/ui/activity/NoticiasActivity.kt
@@ -1,6 +1,7 @@
 package br.com.alura.technews.ui.activity
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -11,13 +12,39 @@ import br.com.alura.technews.ui.extensionsactivity.transacaoFragment
 import br.com.alura.technews.ui.fragment.ListaNoticiasFragment
 import br.com.alura.technews.ui.fragment.VisualizaNoticiaFragment
 
+private const val TAG_FRAGMENT_VISUALIZA_NOTICIA = "visualizaNoticia"
+
 class NoticiasActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_noticias)
-        if (savedInstanceState == null){
+        if (savedInstanceState == null) {
             abreListaNoticias()
+        } else {
+            supportFragmentManager
+                .findFragmentByTag(TAG_FRAGMENT_VISUALIZA_NOTICIA)?.let { fragment ->
+                    val argumentos = fragment.arguments
+                    val novoFragment = VisualizaNoticiaFragment()
+                    novoFragment.arguments = argumentos
+
+                    transacaoFragment {
+                        remove(fragment)
+                    }
+                    supportFragmentManager.popBackStack()
+
+                    transacaoFragment {
+                        val container =
+                            if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                                R.id.activity_noticias_container_secundario
+                            } else {
+                                addToBackStack(null)
+                                R.id.activity_noticias_container_primario
+                            }
+
+                        replace(container, novoFragment, TAG_FRAGMENT_VISUALIZA_NOTICIA)
+                    }
+                }
         }
     }
 
@@ -77,8 +104,15 @@ class NoticiasActivity : AppCompatActivity() {
         fragment.arguments = dados
 
         transacaoFragment {
-            addToBackStack(null)
-            replace(R.id.activity_noticias_container_primario, fragment)
+            val container =
+                if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    R.id.activity_noticias_container_secundario
+                } else {
+                    addToBackStack(null)
+                    R.id.activity_noticias_container_primario
+                }
+
+            replace(container, fragment, TAG_FRAGMENT_VISUALIZA_NOTICIA)
         }
 
     }
